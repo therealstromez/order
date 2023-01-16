9a4c4f3b107b121766454b7db0c8b6ba20b1b88a
stromez <zemorts@gmail.com>
2023-01-16 22:51:10 +0700

    add config

---
+++ static/script/data.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCnWwWFAFmvpoFxRFuCzOs7WkO3odYu8dg",
+    authDomain: "order-app-moi.firebaseapp.com",
+    databaseURL: "https://order-app-moi-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "order-app-moi",
+    storageBucket: "order-app-moi.appspot.com",
+    messagingSenderId: "716520627442",
+    appId: "1:716520627442:web:d7b41b3dfa82cc0cd7446b",
+    measurementId: "G-EMM6X39SSZ"
+};
+firebase.initializeApp(firebaseConfig);
